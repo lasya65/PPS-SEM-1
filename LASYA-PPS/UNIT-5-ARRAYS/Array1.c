d7a0c42f5ccd3f1bb5e175787f9754b06c4f1ed8
lasya65 <sp9174@srmist.edu.in>
2024-10-23 15:24:07 +0530

    more codes added

--- LASYA-PPS/UNIT-5-ARRAYS/Array1.c
+++ LASYA-PPS/UNIT-5-ARRAYS/Array1.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    // Declare a character array to store the registration number
     char regNo[15];
-    
-    // Variable to store each character and length counter
-    char ch;
     int length = 0;
     
-    // Prompt the user to enter the registration number
     printf("Enter the registration number: ");
+    scanf("%s", regNo);
     
-    // Read characters one by one using getchar until a newline ('\n') is encountered
-    while((ch = getchar()) != '\n') {
-        regNo[length] = ch;  // Store the character in the array
-        length++;            // Increment the length counter
+    while (regNo[length] != '\0') {
+        length++;
     }
     
-    // Add null character to mark the end of the string (optional)
-    regNo[length] = '\0'; 
-    
-    // Output the length of the registration number
     printf("The length of the registration number is: %d\n", length);
     
     return 0;
